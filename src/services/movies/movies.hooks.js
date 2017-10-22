034e5fa7d7ecd4f9c5b0c48724aabd0db11cb88e
Matt <matt@ideajunction.uk>
2017-10-22 21:54:04 +0100

    Added populate hooks to movies

--- src/services/movies/movies.hooks.js
+++ src/services/movies/movies.hooks.js
@@ -1,3 +1,30 @@
+const { discard, setByDot } = require('feathers-hooks-common')
+const { shallowPopulate } = require('feathers-shallow-populate')
+
+const getByDotDeep = function (obj, path) {
+  return path.split('.').reduce((obj1, part) => {
+    if (Array.isArray(obj1)) {
+      return obj1.map(item => getByDotDeep(item, part))
+    } else if (typeof obj1 === 'object') {
+      return obj1[part]
+    } else {
+      return undefined
+    }
+  }, obj)
+}
+
+const mapResultData = function (from, to) {
+  return function (context) {
+    const { result } = context
+
+    result.data.forEach(item => {
+      const data = getByDotDeep(item, from)
+      setByDot(item, to, data)
+    })
+
+    return Promise.resolve(context)
+  }
+}
 
 module.exports = {
   before: {
@@ -12,7 +39,30 @@ module.exports = {
 
   after: {
     all: [],
-    find: [],
+    find: [
+      // Populate actors
+      shallowPopulate({
+        include: {
+          service: 'actors',
+          nameAs: 'actors',
+          keyHere: 'actorIds',
+          keyThere: 'actorId'
+        }
+      }),
+      // Map personID
+      mapResultData('actors.personId', 'actors'),
+      // Populate person from actor
+      shallowPopulate({
+        include: {
+          service: 'persons',
+          nameAs: 'persons',
+          keyHere: 'actors',
+          keyThere: '_id'
+        }
+      }),
+      // Remove temp join
+      discard('actors')
+    ],
     get: [],
     create: [],
     update: [],
